47929f0e7d650a352ac1eb354b4ae497cc20d019
lzs <forlzs@163.com>
2023-11-20 22:29:26 +0800

    feature(引入 并使用 stylus):

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -15,27 +15,24 @@ defineProps<{
   </div>
 </template>
 
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
+<style scoped lang="stylus">
+h1
+  font-weight 500
+  font-size 2.6rem
+  position relative
+  top -10px
 
-h3 {
-  font-size: 1.2rem;
-}
+
+h3
+  font-size 1.2rem
+
 
 .greetings h1,
-.greetings h3 {
-  text-align: center;
-}
+.greetings h3
+  text-align center
 
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
+
+@media (min-width: 1024px)
+  .greetings h1, .greetings h3
+    text-align: left
 </style>
