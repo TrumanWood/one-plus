e9bc3ad80d90bca1166b30b14cb103802e16032d
lzs <forlzs@163.com>
2023-11-20 21:57:04 +0800

    feature(优雅打开窗口):

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const { app, BrowserWindow } = require('electron')
+const {app, BrowserWindow} = require('electron')
 const path = require('path')
-const WinState  = require('electron-win-state').default
+const WinState = require('electron-win-state').default
 
 const createWindow = () => {
     const winState = new WinState({
@@ -15,25 +15,32 @@ const createWindow = () => {
         webPreferences: {
             // Electron的 Unable to load preload script 报错解决方案
             // 开启沙盒则preload脚本被禁用，所以得设为false
-            sandbox: false, 
+            sandbox: false,
             // 定义预加载的js
             preload: path.resolve(__dirname, './preload')
-        }
+        },
+        // 设置优雅打开窗口
+        show: false,
     })
-  
+
     win.loadURL('http://localhost:5173')
-    
+
     // open dev tools
     win.webContents.openDevTools();
     winState.manage(win)
-  }
 
-  // 这段程序将会在 Electron 结束初始化
+    // 优雅打开窗口 当内容加载完成之后打开窗口
+    win.on('ready-to-show', () => {
+        win.show();
+    })
+}
+
+// 这段程序将会在 Electron 结束初始化
 // 和创建浏览器窗口的时候调用
 // 部分 API 在 ready 事件触发后才能使用。
-  app.whenReady().then(() => {
+app.whenReady().then(() => {
     createWindow();
-    
+
     app.on('activate', () => {
         // 在 macOS 系统内, 如果没有已开启的应用窗口
         // 点击托盘图标时通常会重新创建一个新窗口
@@ -45,4 +52,4 @@ const createWindow = () => {
     app.on('window-all-closed', () => {
         if (process.platform !== 'darwin') app.quit()
     })
-  })
+})
