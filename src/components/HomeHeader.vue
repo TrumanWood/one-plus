a8524ad9df9b01a63bd52f2272eb1884ec44bf24
lzs <forlzs@163.com>
2023-11-22 22:49:34 +0800

    style(调整代码格式):

---
+++ src/components/HomeHeader.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+  import { ref } from 'vue'
+  const searchValue = ref("");
+  console.log(searchValue);
+</script>
+<template>
+  <div class="header-content">
+    <vs-row>
+      <vs-input v-model="searchValue" color="primary" type="search" placeholder="Search for what you want" >
+        <template #icon>
+          <i class='bx bx-search-alt-2'></i>
+        </template>
+      </vs-input>
+    </vs-row>
+  </div>
+</template>
+
+<style scoped lang="stylus">
+  .header-content,
+  .header-content > vs-row >*
+    width 500px
+</style>
